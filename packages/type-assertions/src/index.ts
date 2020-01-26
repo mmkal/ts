333f8bec51b0e934ca5bb6f367febf87f9756265
mmkal <misha.kaletsky@gmail.com>
2020-01-25 22:14:12 -0500

    feat: more basic type assertions

--- packages/type-assertions/src/index.ts
+++ packages/type-assertions/src/index.ts
@@ -21,6 +21,15 @@ export interface ExpectTypeOf<Actual, B extends boolean> {
   toBeAny: (...MISMATCH: MismatchArgs<IsAny<Actual>, B>) => true
   toBeUnknown: (...MISMATCH: MismatchArgs<IsUnknown<Actual>, B>) => true
   toBeNever: (...MISMATCH: MismatchArgs<IsNever<Actual>, B>) => true
+  toBeFunction: (...MISMATCH: MismatchArgs<Extends<Actual, (...args: any[]) => any>, B>) => true
+  toBeObject: (...MISMATCH: MismatchArgs<Extends<Actual, object>, B>) => true
+  toBeArray: (...MISMATCH: MismatchArgs<Extends<Actual, any[]>, B>) => true
+  toBeNumber: (...MISMATCH: MismatchArgs<Extends<Actual, number>, B>) => true
+  toBeString: (...MISMATCH: MismatchArgs<Extends<Actual, string>, B>) => true
+  toBeBoolean: (...MISMATCH: MismatchArgs<Extends<Actual, boolean>, B>) => true
+  toBeSymbol: (...MISMATCH: MismatchArgs<Extends<Actual, Symbol>, B>) => true
+  toBeNull: (...MISMATCH: MismatchArgs<Extends<Actual, null>, B>) => true
+  toBeUndefined: (...MISMATCH: MismatchArgs<Extends<Actual, undefined>, B>) => true
   toMatchTypeOf: <Expected>(expected?: Expected, ...MISMATCH: MismatchArgs<Extends<Actual, Expected>, B>) => true
   toEqualTypeOf: <Expected>(
     expected?: Expected,
@@ -40,20 +49,36 @@ export interface ExpectTypeOf<Actual, B extends boolean> {
   parameter<K extends keyof Params<Actual>>(number: K): ExpectTypeOf<Params<Actual>[K], B>
   returns: Actual extends (...args: any[]) => infer R ? ExpectTypeOf<R, B> : never
   resolves: Actual extends PromiseLike<infer R> ? ExpectTypeOf<R, B> : never
+  items: Actual extends ArrayLike<infer R> ? ExpectTypeOf<R, B> : never
   not: ExpectTypeOf<Actual, Not<B>>
 }
 const fn: any = () => true
 export const expectTypeOf = <Actual>(actual?: Actual): ExpectTypeOf<Actual, true> => {
-  const obj = {
+  const nonFunctionProperties = ['returns', 'resolves', 'not', 'items'] as const
+  type Keys = keyof ExpectTypeOf<any, any>
+
+  type FunctionsDict = Record<Exclude<Keys, (typeof nonFunctionProperties)[number]>, any>
+  const obj: FunctionsDict = {
     toBeAny: fn,
     toBeUnknown: fn,
     toBeNever: fn,
+    toBeFunction: fn,
+    toBeObject: fn,
+    toBeArray: fn,
+    toBeString: fn,
+    toBeNumber: fn,
+    toBeBoolean: fn,
+    toBeSymbol: fn,
+    toBeNull: fn,
+    toBeUndefined: fn,
     toMatchTypeOf: fn,
     toEqualTypeOf: fn,
     toBeCallableWith: fn,
     parameter: expectTypeOf,
   }
-  const properties: Array<keyof ExpectTypeOf<any, true>> = ['returns', 'resolves', 'not']
-  properties.forEach(prop => Object.defineProperty(obj, prop, {get: () => expectTypeOf({})}))
+
+  const getterProperties: readonly Keys[] = nonFunctionProperties
+  getterProperties.forEach((prop: Keys) => Object.defineProperty(obj, prop, {get: () => expectTypeOf({})}))
+
   return obj as any
 }
