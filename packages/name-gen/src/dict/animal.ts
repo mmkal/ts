cc453849519056c8daecbc5d2b8a572dd70af6ab
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2020-02-08 13:05:57 -0500

    feat(animals): use wikipedia as source (#9)

--- packages/name-gen/src/dict/animal.ts
+++ packages/name-gen/src/dict/animal.ts
@@ -1,316 +1,271 @@
-// from https://web.archive.org/web/20190611082829/https://www.alphalists.com/list/alphabetical-list-animals
+// from https://web.archive.org/web/20200105051826/https://en.wikipedia.org/wiki/List_of_animal_names
+// ran in dev tools: [...document.querySelectorAll('table.wikitable')[1].querySelectorAll('tr')].slice(1).map(td => td.querySelector('a')).filter(Boolean).map(a => a.innerText).filter(Boolean).map(t => t.split(' (')[0]).filter(Boolean).filter(t => !t.includes('Wikipedia')).join('\n')
 
 export const animal = `
 Aardvark
-Aardwolf
-African Elephant
-African Tree Pangolin
+Albatross
 Alligator
 Alpaca
+Ant
 Anteater
 Antelope
 Ape
-Arabian Horse
 Armadillo
-Arthropod
-Asian Elephant
-Aye-Aye
+Ass/donkey
 Baboon
 Badger
-Bandicoot
-Bangle Tiger
+Barracuda
 Bat
-Bearded Dragon
+Bear
 Beaver
-Beluga Whale
-Bengal Tiger
-Big-Horned Sheep
-Billy Goat
+Bee
+Binturong
 Bird
 Bison
-Black Bear
-Black Footed Rhino
-Black Howler Monkey
-Black Rhino
-Blacktip Reef Shark
-Blue Shark
-Blue Whale
+Bluebird
 Boar
-Bob-Cat
-Bonobo
-Bottlenose Dolphin
-Bottlenose Whale
-Brown Bear
+Bobcat
 Buffalo
-Bull
-Bull frog
-Caiman Lizard
+Butterfly
 Camel
-Capuchin Monkey
 Capybara
+Caracal
 Caribou
+Cassowary
 Cat
+Caterpillar
+Cattle
+Chameleon
+Chamois
 Cheetah
+Chicken
 Chimpanzee
 Chinchilla
-Chipmunk
-Common Dolphin
-Common Seal
-Coral and Anemone
+Chough
+Coati
+Cobra
+Cockroach
+Cod
+Cormorant
 Cougar
-Cow
 Coyote
+Crab
+Crane
+Cricket
 Crocodile
-Crustacean
-Dart Frog
+Crow
+Cuckoo
+Curlew
 Deer
-Degus
-Dik-Dik
+Degu
+Dhole
 Dingo
+Dinosaur
 Dog
-Dogfish Shark
+Dogfish
 Dolphin
 Donkey
-Door Mouse
-Dormouse
-Draft Horse
-Duckbill Platypus
+Dotterel
+Dove
+Dragonfly
+Duck
 Dugong
-Dusky Shark
+Dunlin
+Eagle
+Echidna
+Eel
+Eland
 Elephant
-Elephant Seal
+Elephant seal
 Elk
-Ermine
-Eurasian Lynx
+Emu
+Falcon
 Ferret
+Finch
 Fish
-Florida Panther
-Flying Fox
+Flamingo
+Fly
 Fox
-Fresh Water Crocodile
 Frog
-Fur Seal
-Galapagos Land Iguana
-Galapagos Shark
-Galapagos Tortoise
+Gaur
 Gazelle
 Gecko
-Giant Anteater
-Giant Panda
-Gibbon
+Gerbil
+Giant panda
 Giraffe
+Gnat
+Gnu
 Goat
-Gopher
+Goldfinch
+Goosander
+Goose
 Gorilla
-Gray Fox
-Gray Nurse Shark
-Gray Reef Shark
-Gray Whale
-Great White Shark
-Green Poison Dart Frog
-Green Sea Turtle
-Grizzly Bear
-Groundhog
-Guinea Pig
-Hairy-Nosed Wombat
-Hammerhead Shark
-Harbor Porpoise
-Harbor Seal
+Goshawk
+Grasshopper
+Grouse
+Guanaco
+Guinea fowl
+Guinea pig
+Gull
+Hamster
 Hare
-Harp Seal
-Hawaiian Monk Seal
+Hawk
 Hedgehog
+Hermit crab
+Heron
+Herring
 Hippopotamus
-Horn Shark
+Hoatzin
+Hoopoe
+Hornet
 Horse
-Howler Monkey
-Humpback Whale
+Human
+Hummingbird
 Hyena
-Hyrax
+Ibex
+Ibis
 Iguana
-Iguanodon
 Impala
-Insect
-Irrawaddy Dolphin
 Jackal
-Jackrabbit
 Jaguar
+Jay
 Jellyfish
+Jerboa
 Kangaroo
-Kermode Bear
-Killer Whale
+Kingfisher
+Kinkajou
 Koala
-Komodo Dragon
+Komodo dragon
 Kookaburra
-Lama
-Lamb
-Lancelet
-Least Weasel
-Leatherback Sea Turtle
-Lemming
-Lemon Shark
+Kouprey
+Kudu
+Lapwing
+Lark
 Lemur
 Leopard
-Leopard Gecko
-Leopard Seal
-Leopard Shark
 Lion
+Lizard
 Llama
-Loggerhead Turtles
+Lobster
+Locust
+Loris
+Louse
 Lynx
-Mako Shark
+Lyrebird
+Macaque
+Macaw
+Magpie
+Mallard
+Mammoth
 Manatee
-Manta Ray
-Mantis
-Marbled Salamander
+Mandrill
+Marmoset
 Marmot
-Marsupial
 Meerkat
-Megamouth Shark
-Melon-Headed Whale
-Miniature Donkey
 Mink
-Minke Whale
 Mole
-Mole Salamander
-Mollusk
 Mongoose
-Monitor Lizard
-Monk Seal
 Monkey
 Moose
-Mountain Lion
+Mosquito
 Mouse
-Mule
-Muskox
-Muskrat
-Naked Mole Rat
+Myna
 Narwhal
-Nautilus
 Newt
-Northern Right Whale
-Nurse Shark
-Nutria
-Nyala
-Ocelot
+Nightingale
+Nine-banded armadillo
 Octopus
 Okapi
 Opossum
 Orangutan
-Orca
+Oryx
+Ostrich
 Otter
-Ox
-Panda
+Owl
+Oyster
 Panther
+Parrot
+Panda
+Partridge
+Peafowl
+Pelican
+Penguin
+Pheasant
 Pig
-Pilot Whale
-Pine Marten
-Platypus
-Polar Bear
+Pigeon
+Pika
+Polar bear
+Pony
 Porcupine
 Porpoise
-Possum
-Potbellied Pig
-Potto
-Prairie Dog
-Proboscis Monkey
-Przewalski's Wild horse
-Puma
-Pygmy Hippopotamus
-Pygmy Right Whale
-Pygmy Sperm Whale
-Quokkas
-Quolls
+Prairie dog
+Pug
+Quail
+Quelea
+Quetzal
 Rabbit
 Raccoon
+Ram
 Rat
-Ray
-Red Fox
-Red Kangaroo
-Red Panda
-Red-Eyed Tree Frog
-Reef Shark
+Raven
+Red deer
+Red panda
 Reindeer
-Rhino
+Rhea
 Rhinoceros
-Right Whale
-Ringed Seal
-Risso's Dolphin
-River Dolphin
+Rook
 Salamander
-Sandbar Shark
-Scalloped Hammerhead Shark
-Sea Lion
-Sea Turtles
+Salmon
+Sand dollar
+Sandpiper
+Sardine
+Sea lion
+Seahorse
 Seal
-Sei Whale
 Shark
 Sheep
-Shortfin Mako Shark
-Siberian Tiger
-Silky Shark
-Skink
+Shrew
+Siamang
 Skunk
-Slender Loris
 Sloth
-Sloth Bear
+Snail
 Snake
-Snow Fox
-Snow Hare
-Snow Leopard
-Snow Monkey
-Somali Wild Ass
-Spectacled Bear
-Sperm Whale
-Spider Monkey
-Spiny Dogfish Shark
-Spotted Dolphin
+Spider
+Squid
 Squirrel
-Star-Nosed Mole
-Stellar Sea Lion
-Striped Dolphin
-Sun Bear
-Takin
+Starling
+Stegosaurus
+Swan
 Tamarin
 Tapir
-Tasmanian Devil
-Tasmanian Tiger
-Terrapin
-Thresher Shark
+Tarsier
+Termite
 Tiger
-Tiger Salamander
-Tiger Shark
-Topi
-Tortoise
-Tree Frog
+Toad
+Toucan
+Turaco
+Turkey
 Turtle
-Uakari
-Vampire Bat
-Vancouver Island Marmot
-Vervet Monkey
-Vicuna
-Vole
+Umbrellabird
+Vicuña
+Vinegaroon
+Viper
+Vulture
 Wallaby
 Walrus
-Warthog
-Water Buffalo
+Wasp
+Water buffalo
+Waxwing
 Weasel
-Whale Shark
 Whale
-White Rhino
-White Tailed Deer
-White-Beaked Dolphin
-Whitetip Reef Shark
-Wildcat
-Wildebeest
-Wobbegong Shark
+Wobbegong
 Wolf
 Wolverine
 Wombat
-Woodchuck
+Woodpecker
+Worm
+Wren
+X-ray tetra
 Yak
-Yellow-Bellied Marmot
 Zebra
-Zebu
-Zorilla
 `.split('\n')
