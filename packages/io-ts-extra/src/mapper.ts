47f6c9f76adfa76102f24406db6a6fb3acb439a5
mmkal <misha.kaletsky@gmail.com>
2020-02-29 20:37:33 -0500

    docs: mapper and parser docs

--- packages/io-ts-extra/src/mapper.ts
+++ packages/io-ts-extra/src/mapper.ts
@@ -9,6 +9,30 @@ interface Mapper {
   <From, To>(from: t.Type<From>, to: t.Type<To>, map: (f: From) => To): Decoder<To, From> & {from: From; to: To}
   <From, To>(from: t.Type<From>, to: t.Type<To>, map: (f: From) => To, unmap: (t: To) => From): t.Type<To, From, From> & {from: From; to: To}
 }
+
+/**
+ * A helper for building "parser-decoder" types - that is, types that validate an input,
+ * transform it into another type, and then validate the target type.
+ *
+ * @example
+ * const StringsFromMixedArray = mapper(
+ *   t.array(t.any),
+ *   t.array(t.string),
+ *   mixedArray => mixedArray.filter(value => typeof value === 'string')
+ * )
+ * StringsFromMixedArray.decode(['a', 1, 'b', 2]) // right(['a', 'b'])
+ * StringsFromMixedArray.decode('not an array')   // left(...)
+ * const IntFromString = mapper(t.string, t.Integer, parseFloat)
+ * IntFromString.decode('123')          // right(123)
+ * IntFromString.decode('123.4')        // left(...)
+ * IntFromString.decode('not a number') // left(...)
+ * IntFromString.decode(123)            // left(...)
+ *
+ * @param from the expected type of input value
+ * @param to the expected type of the decoded value
+ * @param map transform (decode) a `from` type to a `to` type
+ * @param unmap transfrom a `to` type back to a `from` type
+ */
 export const mapper: Mapper = <From, To>(
   from: t.Type<From>,
   to: t.Type<To>,
@@ -33,5 +57,20 @@ export const mapper: Mapper = <From, To>(
   return Object.assign(piped, {from, to})
 }
 
+/**
+ * A helper for parsing strings into other types. A wrapper around `mapper` where the `from` type is `t.string`.
+ * @see mapper
+ *
+ * @example
+ * const IntFromString = parser(t.string, parseFloat)
+ * IntFromString.decode('123')          // right(123)
+ * IntFromString.decode('123.4')        // left(...)
+ * IntFromString.decode('not a number') // left(...)
+ * IntFromString.decode(123)            // left(...)
+ *
+ * @param type the target type
+ * @param decode transform a string into the target type
+ * @param encode transform the target type back into a string
+ */
 export const parser = <T>(type: t.Type<T>, decode: (value: string) => T, encode: (value: T) => string = String) =>
   mapper(t.string, type, decode, encode)
