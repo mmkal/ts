c9c39cff828d632b42fade7f2098516514ac95fd
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2020-06-10 07:33:01 -0400

    chore(deps): update eslint packages (#50)
    * chore(deps): update eslint packages
    
    * fix: new lint errors

--- packages/io-ts-extra/src/reporters.ts
+++ packages/io-ts-extra/src/reporters.ts
@@ -12,7 +12,7 @@ export const validationErrors = (validation: t.Validation<unknown>, typeAlias?: 
     return ['No errors!']
   }
   return validation.left.map(e => {
-    const name = typeAlias || (e.context[0] && e.context[0].type.name)
+    const name = typeAlias || e.context[0]?.type.name
     const lastType = e.context.length && e.context[e.context.length - 1].type.name
     const path = name + e.context.map(c => c.key).join('.')
     return `Invalid value {${inspect(e.value)}} supplied to ${path}. Expected ${lastType}.`
