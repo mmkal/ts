922d7550a34acbb5730056d48228122a6ee141c0
mmkal <misha.kaletsky@gmail.com>
2020-07-08 01:03:41 +0100

    chore: dedicated eslint tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  parserOptions: {ecmaVersion: 2018, sourceType: 'module', extraFileExtensions: ['.md']},
-  plugins: [
-    '@typescript-eslint/eslint-plugin',
-    'prettier',
-    'codegen',
-    'unicorn',
-    'jest',
-    'import',
-  ],
-  env: { 'jest/globals': true, node: true },
+  parserOptions: {
+    project: './tsconfig.eslint.json', // https://github.com/typescript-eslint/typescript-eslint/issues/967#issuecomment-530907956
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    extraFileExtensions: ['.md', '.yml'],
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier', 'codegen', 'unicorn', 'jest', 'import'],
+  env: {'jest/globals': true, node: true},
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
@@ -20,26 +18,34 @@ module.exports = {
     'xo',
     'xo-typescript',
   ],
-  ignorePatterns: ['dist', 'node_modules', 'coverage', '.eslintrc.js'],
+  ignorePatterns: [
+    'dist',
+    'node_modules',
+    'coverage',
+    '!.github', // https://github.com/eslint/eslint/issues/8429#issuecomment-355967308
+  ],
   rules: {
     'prettier/prettier': ['warn', require('./.prettierrc')],
     'codegen/codegen': ['warn', {presets: {badges: require('./scripts/badges')}}],
-    
+
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/prefer-readonly-parameter-types': 'off',	
-    '@typescript-eslint/no-unsafe-member-access': 'off',	
-    '@typescript-eslint/no-unsafe-call': 'off',	
+    '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/unified-signatures': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
 
-    '@typescript-eslint/no-unused-vars': ['error', {
-      varsIgnorePattern: '^_',
-      argsIgnorePattern: '^_',
-      caughtErrorsIgnorePattern: '^_',
-      ignoreRestSiblings: true,
-      args: 'after-used',
-    }],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+        args: 'after-used',
+      },
+    ],
 
     '@typescript-eslint/ban-ts-comment': ['error', {'ts-expect-error': false}],
     '@typescript-eslint/no-invalid-void-type': 'off',
@@ -50,28 +56,33 @@ module.exports = {
     'operator-linebreak': 'off',
     'no-mixed-spaces-and-tabs': 'off',
     '@typescript-eslint/indent': 'off',
-    'indent': 'off',
-    'semi': 'off',
-    'quotes': 'off',
+    indent: 'off',
+    semi: 'off',
+    quotes: 'off',
     '@typescript-eslint/semi': 'off',
     '@typescript-eslint/quotes': 'off',
     'eol-last': 'off',
     'no-trailing-spaces': 'off',
     '@typescript-eslint/member-delimiter-style': 'off',
-    'camelcase': 'off',
+    camelcase: 'off',
 
     'capitalized-comments': 'off',
 
     'jest/expect-expect': [
-      'error',
-      {assertFunctionNames: ['expect', 'expectTypeOf', 'expectLeft', 'expectRight']}
+      'error', // br
+      {assertFunctionNames: ['expect', 'expectTypeOf', 'expectLeft', 'expectRight']},
     ],
 
     'no-else-return': ['warn', {allowElseIf: true}],
 
-    '@typescript-eslint/naming-convention': ['warn', {
-      selector: 'variableLike', format: ['camelCase', 'PascalCase'], leadingUnderscore: 'allow',
-    }],
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: 'variableLike',
+        format: ['camelCase', 'PascalCase'],
+        leadingUnderscore: 'allow',
+      },
+    ],
 
     // maybe turn on later?
     'padding-line-between-statements': 'off',
@@ -112,13 +123,13 @@ module.exports = {
       parserOptions: {sourceType: 'script'},
       rules: {
         '@typescript-eslint/no-var-requires': 'off',
-      }
+      },
     },
     {
       files: ['**/*.md'],
       rules: {
         'unicorn/filename-case': 'off',
-      }
+      },
     },
-  ]
+  ],
 }
