659dec36a8d1e2e492b78a3a73c17883a23a0fec
mmkal <misha.kaletsky@gmail.com>
2020-09-17 21:47:51 -0400

    Port check-clean to typescript

---
+++ packages/check-clean/src/__tests__/index.test.ts
@@ -0,0 +1,57 @@
+import * as childProcess from 'child_process'
+import {checkClean} from '..'
+
+jest.mock('child_process')
+
+const execSync = jest.spyOn(childProcess, 'execSync')
+
+const logs = [] as any[]
+const consoleInfo = jest.spyOn(console, 'info').mockImplementation((...args) => logs.push(['info', ...args]))
+const consoleError = jest.spyOn(console, 'error').mockImplementation((...args) => logs.push(['error', ...args]))
+
+beforeEach(() => {
+  jest.clearAllMocks()
+  logs.splice(0, logs.length)
+})
+
+test('checks using git status', () => {
+  execSync.mockReturnValue(Buffer.from('\n'))
+  const exit: any = jest.fn()
+
+  checkClean({exit})
+
+  expect(execSync).toHaveBeenCalledTimes(1)
+  expect(execSync).toHaveBeenCalledWith('git status --porcelain')
+
+  expect(exit).not.toHaveBeenCalled()
+  expect(consoleInfo).not.toHaveBeenCalled()
+  expect(consoleError).not.toHaveBeenCalled()
+})
+
+test('logs and exits with error code if there are changes', () => {
+  execSync.mockReturnValue(Buffer.from('A some/file.txt'))
+  const exit: any = jest.fn()
+
+  checkClean({exit})
+
+  expect(exit).toHaveBeenCalledTimes(1)
+  expect(exit).toHaveBeenCalledWith(1)
+
+  expect(logs).toMatchInlineSnapshot(`
+    Array [
+      Array [
+        "error",
+        "[31merror: git changes detected",
+      ],
+      Array [
+        "error",
+        "[31mcheck them in before running again[0m",
+      ],
+      Array [
+        "info",
+        "[0mchanges:
+    A some/file.txt",
+      ],
+    ]
+  `)
+})
---
+++ packages/check-clean/src/index.ts
@@ -0,0 +1,26 @@
+import * as childProcess from 'child_process'
+
+const colours = {
+  red: '\u001B[31m',
+  yellow: '\u001B[33m',
+  reset: '\u001B[0m',
+}
+
+export const checkClean = ({exit = process.exit} = {}) => {
+  const gitStatus = childProcess.execSync('git status --porcelain').toString().trim()
+  if (!gitStatus) {
+    return
+  }
+  console.error(`${colours.red}error: git changes detected`)
+  console.error(
+    process.env.CI
+      ? `${colours.yellow}this was run in a CI environment, you probably don't want changes to have been generated here. Try to reproduce this locally, and check the changes in before re-running in CI${colours.reset}`
+      : `${colours.red}check them in before running again${colours.reset}`
+  )
+
+  if (process.argv[2]) {
+    console.info(`${colours.reset}additional info: ${process.argv[2]}`)
+  }
+  console.info(`${colours.reset}changes:\n${gitStatus}`)
+  exit(1)
+}
