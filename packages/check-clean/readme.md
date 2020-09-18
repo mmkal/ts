5877dd069e20dfe0633e31c42c46597054f5e881
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2020-09-17 23:09:32 -0400

    Port check-clean to typescript (#167)
    * Port check-clean to typescript
    
    * chore: change files
    
    * verbose
    
    * fix: ci test
    
    * fix: lint errors
    
    Co-authored-by: mmkal <mmkal@users.noreply.github.com>

---
+++ packages/check-clean/readme.md
@@ -0,0 +1,21 @@
+# check-clean
+
+A cli tool to make sure you have no git changes.
+
+## Usage
+
+```bash
+npx check-clean
+```
+
+## API
+
+```bash
+npm install check-clean
+```
+
+```js
+const {checkClean} = require('check-clean')
+
+checkClean()
+```
---
+++ packages/check-clean/src/__tests__/index.test.ts
@@ -0,0 +1,85 @@
+import * as childProcess from 'child_process'
+import {checkClean} from '..'
+
+jest.mock('child_process')
+
+const execSync = jest.spyOn(childProcess, 'execSync')
+
+const logs = [] as any[]
+const consoleInfo = jest.spyOn(console, 'info').mockImplementation((...args) => logs.push(['info', ...args]))
+const consoleError = jest.spyOn(console, 'error').mockImplementation((...args) => logs.push(['error', ...args]))
+
+beforeEach(() => {
+  jest.clearAllMocks()
+  logs.splice(0, logs.length)
+})
+
+test('checks using git status', () => {
+  execSync.mockReturnValue(Buffer.from('\n'))
+  const exit: any = jest.fn()
+
+  checkClean({exit, env: {}, argv: []})
+
+  expect(execSync).toHaveBeenCalledTimes(1)
+  expect(execSync).toHaveBeenCalledWith('git status --porcelain')
+
+  expect(exit).not.toHaveBeenCalled()
+  expect(consoleInfo).not.toHaveBeenCalled()
+  expect(consoleError).not.toHaveBeenCalled()
+})
+
+test('logs and exits with error code if there are changes', () => {
+  execSync.mockReturnValue(Buffer.from('A some/file.txt'))
+  const exit: any = jest.fn()
+
+  checkClean({exit, env: {CI: ''}, argv: []})
+
+  expect(exit).toHaveBeenCalledTimes(1)
+  expect(exit).toHaveBeenCalledWith(1)
+
+  expect(logs).toMatchInlineSnapshot(`
+    Array [
+      Array [
+        "error",
+        "[31merror: git changes detected",
+      ],
+      Array [
+        "error",
+        "[31mcheck them in before running again[0m",
+      ],
+      Array [
+        "info",
+        "[0mchanges:
+    A some/file.txt",
+      ],
+    ]
+  `)
+})
+
+test('logs and exits with error code if there are changes in CI', () => {
+  execSync.mockReturnValue(Buffer.from('A some/file.txt'))
+  const exit: any = jest.fn()
+
+  checkClean({exit, env: {CI: 'true'}, argv: []})
+
+  expect(exit).toHaveBeenCalledTimes(1)
+  expect(exit).toHaveBeenCalledWith(1)
+
+  expect(logs).toMatchInlineSnapshot(`
+    Array [
+      Array [
+        "error",
+        "[31merror: git changes detected",
+      ],
+      Array [
+        "error",
+        "[33mthis was run in a CI environment, you probably don't want changes to have been generated here. Try to reproduce this locally, and check the changes in before re-running in CI[0m",
+      ],
+      Array [
+        "info",
+        "[0mchanges:
+    A some/file.txt",
+      ],
+    ]
+  `)
+})
